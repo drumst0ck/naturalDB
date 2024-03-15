a79e7b484cad74def27cf51847870116f0be823e
Jose Sánchez <drumstock@protonmail.com>
2024-03-15 14:59:31 +0100

    rt

---
+++ app/api/route.js
@@ -0,0 +1,42 @@
+import { SqlDatabase } from "langchain/sql_db";
+import { DataSource } from "typeorm";
+import { ChatOpenAI } from "@langchain/openai";
+import { NextResponse } from "next/server";
+import { createSqlQueryChain } from "langchain/chains/sql_db";
+import { QuerySqlTool } from "langchain/tools/sql";
+export async function POST(req, res, next) {
+  const llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 });
+  /*   const MysqlDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "test",
+    password: "test",
+    database: "test",
+  }); */
+
+  const postgresDataSource = new DataSource({
+    type: "postgres",
+    host: "157.90.123.33",
+    port: 9002,
+    username: "postgres",
+    password: "L4QysZYDFqN0Kbw2tVPpK1CsHMevhRP3",
+    database: "postgres",
+  });
+  const db = await SqlDatabase.fromDataSourceParams({
+    appDataSource: postgresDataSource,
+  });
+  const executeQuery = new QuerySqlTool(db);
+  const writeQuery = await createSqlQueryChain({
+    llm,
+    db,
+    dialect: "postgres",
+  });
+  const chain = writeQuery.pipe(executeQuery);
+  const response = await chain.invoke({
+    question: "Cual es el usuario que mas mensajes ha mandado?",
+  });
+  console.log(response);
+
+  return NextResponse.json(response);
+}
