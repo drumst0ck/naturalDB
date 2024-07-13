96bf19d7abb0acc4dfc1b88633fa25e34af6b52e
Jose Sánchez <drumstock@protonmail.com>
2024-07-13 12:24:16 +0200

    🔇 (route.js): Remove console.log statements for POST request in get-db-schema route 🔇 (getDBSchema.js): Remove console.log statement from getPostgresSchema function

--- app/api/get-db-schema/route.js
+++ app/api/get-db-schema/route.js
@@ -3,12 +3,8 @@ import getDBSchema from "../../../lib/getDBSchema";
 
 export async function POST(req) {
   try {
-    console.log("Iniciando POST request");
     const dbConfig = await req.json();
-    console.log("dbConfig recibido:", dbConfig);
-    console.log("Llamando a getDBSchema");
     const schema = await getDBSchema(dbConfig);
-    console.log("Schema obtenido:", schema);
     return NextResponse.json(schema);
   } catch (error) {
     console.error("Error getting DB schema:", error);
