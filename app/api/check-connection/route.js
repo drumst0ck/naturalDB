1ffccc3ec1937ad9f14091da5d13c3975b1bb1b4
Jose Sánchez <drumstock@protonmail.com>
2024-07-13 11:45:16 +0200

    🐛 (route.js): Fix connection logic for PostgreSQL and handle unsupported database types in the POST request handler.

--- app/api/check-connection/route.js
+++ app/api/check-connection/route.js
@@ -1,49 +1,33 @@
 import { DataSource } from "typeorm";
 import { NextResponse } from "next/server";
-import mysql from 'mysql2/promise';
 
 export async function POST(req) {
-    const { type, host, username, port, database, password } = await req.json();
+  const { type, host, username, port, database, password } = await req.json();
 
-    if (type === 'mysql') {
-        try {
-            const connection = await mysql.createConnection({
-                host,
-                user: username,
-                password,
-                database,
-                port,
-            });
+  if (type === "postgres") {
+    const dataSourceOptions = {
+      type: "postgres",
+      host,
+      port,
+      username,
+      password,
+      database,
+      synchronize: false,
+    };
 
-            await connection.connect();
-            await connection.end();
-
-            return NextResponse.json({ connected: true });
-        } catch (error) {
-            console.error('MySQL connection error:', error);
-            return NextResponse.json({ connected: false, error: error.message });
-        }
-    } else if (type === 'postgres') {
-        const dataSourceOptions = {
-            type: 'postgres',
-            host,
-            port,
-            username,
-            password,
-            database,
-            synchronize: false,
-        };
-
-        try {
-            const dataSource = new DataSource(dataSourceOptions);
-            await dataSource.initialize();
-            await dataSource.destroy();
-            return NextResponse.json({ connected: true });
-        } catch (error) {
-            console.error('PostgreSQL connection error:', error);
-            return NextResponse.json({ connected: false, error: error.message });
-        }
-    } else {
-        return NextResponse.json({ connected: false, error: 'Unsupported database type' }, { status: 400 });
+    try {
+      const dataSource = new DataSource(dataSourceOptions);
+      await dataSource.initialize();
+      await dataSource.destroy();
+      return NextResponse.json({ connected: true });
+    } catch (error) {
+      console.error("PostgreSQL connection error:", error);
+      return NextResponse.json({ connected: false, error: error.message });
     }
+  } else {
+    return NextResponse.json(
+      { connected: false, error: "Unsupported database type" },
+      { status: 400 }
+    );
+  }
 }
