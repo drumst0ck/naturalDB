0c07b9d3f74f1e500616554ad050e3c5ad38def6
Jose Sánchez <drumstock@protonmail.com>
2024-07-13 12:27:29 +0200

    🐛 (route.js): Fix incorrect quotation marks and formatting for error messages 🔧 (route.js): Update Pool configuration to properly connect to the database using provided credentials

--- app/api/execute-query/route.js
+++ app/api/execute-query/route.js
@@ -1,17 +1,30 @@
-import { NextResponse } from 'next/server';
-import { Pool } from 'pg';
+import { NextResponse } from "next/server";
+import { Pool } from "pg";
 export async function POST(req) {
-    try {
-        const { query, dbConfig } = await req.json();
-        if (!/^SELECT/i.test(query.trim())) {
-            return NextResponse.json({ error: 'Solo se permiten consultas SELECT' }, { status: 400 });
-        }
-        const pool = new Pool(dbConfig);
-        const result = await pool.query(query);
-        await pool.end();
-        return NextResponse.json(result.rows);
-    } catch (error) {
-        console.error('Error executing query:', error);
-        return NextResponse.json({ error: 'Error al ejecutar la consulta' }, { status: 500 });
+  try {
+    const { query, dbConfig } = await req.json();
+    if (!/^SELECT/i.test(query.trim())) {
+      return NextResponse.json(
+        { error: "Solo se permiten consultas SELECT" },
+        { status: 400 }
+      );
     }
+    const pool = new Pool({
+      user: dbConfig.username,
+      host: dbConfig.host,
+      database: dbConfig.database,
+      password: dbConfig.password,
+      port: parseInt(dbConfig.port),
+      ssl: false,
+    });
+    const result = await pool.query(query);
+    await pool.end();
+    return NextResponse.json(result.rows);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    return NextResponse.json(
+      { error: "Error al ejecutar la consulta" },
+      { status: 500 }
+    );
+  }
 }
