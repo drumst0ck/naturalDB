7b7272c4d8d52facd84f5b8378dd9e547cbe5631
Jose Sánchez <drumstock@protonmail.com>
2024-07-13 12:02:43 +0200

    📝 (route.js): update POST endpoint to call getDBSchema with test database configuration instead of dynamic user input for testing purposes

--- app/api/get-db-schema/route.js
+++ app/api/get-db-schema/route.js
@@ -7,7 +7,16 @@ export async function POST(req) {
     const dbConfig = await req.json();
     console.log("dbConfig recibido:", dbConfig);
     console.log("Llamando a getDBSchema");
-    const schema = await getDBSchema(dbConfig);
+    const testDB = {
+      type: "test",
+      host: "157.90.123.33",
+      username: "drumstock",
+      password: "test123",
+      port: 10299,
+      database: "test",
+      type: "postgres",
+    };
+    const schema = await getDBSchema(testDB);
     console.log("Schema obtenido:", schema);
     return NextResponse.json(schema);
   } catch (error) {
