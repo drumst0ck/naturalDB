b0932e5f5831fd2c473a85432f5a0877511ea591
Jose Sánchez <drumstock@protonmail.com>
2024-07-31 14:48:51 +0200

    🔥 (Home.jsx): Remove unused next/image import to optimize code and improve performance

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import Image from "next/image";
 
 export default function Home() {
   return (
